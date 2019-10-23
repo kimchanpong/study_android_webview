697fcaa2346de1d9d0c5f35ceb6a5f91e9290f9e
kimchanpong <rlacksgh0826@gmail.com>
2019-10-23 09:14:17 +0900

    add : chanpong-blog webview app

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.webview;
+
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import static com.example.webview.R.id.webView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(webView);
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("http://chanpong.win");
+        mWebView.setWebChromeClient(new WebChromeClient());
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {//웹뷰에서 뒤로가기 버튼을 누르면 뒤로가짐
+            mWebView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class WebViewClientClass extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
