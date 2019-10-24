b71e4c265152617f358d568f26433be12af67ee2
kimchanpong <rlacksgh0826@gmail.com>
2019-10-24 15:37:29 +0900

    modify : progressbar 적용

--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.webview;
 
 import android.annotation.TargetApi;
+import android.graphics.Bitmap;
 import android.os.Build;
 import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import static com.example.webview.R.id.webView;
@@ -14,6 +17,7 @@ import static com.example.webview.R.id.webView;
 public class MainActivity extends AppCompatActivity {
 
     private WebView mWebView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +26,37 @@ public class MainActivity extends AppCompatActivity {
         mWebView = (WebView) findViewById(webView);
 
         mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.loadUrl("http://chanpong.win");
         mWebView.setWebChromeClient(new WebChromeClient());
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+                progressBar.setVisibility(View.GONE);
+            }
+
+//            @TargetApi(Build.VERSION_CODES.N)
+//            @Override
+//            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+//                String url = request.getUrl().toString();
+//                view.loadUrl(url);
+//                return true;
+//            }
+        });
+
+
+        //progressbar
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+        progressBar.setVisibility(View.GONE);
+
+        mWebView.loadUrl("http://chanpong.win");
     }
 
     @Override
@@ -34,16 +67,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onKeyDown(keyCode, event);
-    }
-
-    private class WebViewClientClass extends WebViewClient {
-
-        @TargetApi(Build.VERSION_CODES.N)
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-            String url = request.getUrl().toString();
-            view.loadUrl(url);
-            return true;
-        }
     }
 }
